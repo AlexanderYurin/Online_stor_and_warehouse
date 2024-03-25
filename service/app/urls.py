e1aba1f3ec1a8b26bfc624fe620340f0692021a8
Alexander Yurin <hiimmandarin32@gmail.com>
2024-03-25 17:28:41 +0300

    Fix for tests

--- service/app/urls.py
+++ service/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from app.views import get_total_orders
 
 urlpatterns = [
-    path("", get_total_orders.as_view()),
+    path("", get_total_orders.as_view(), name="main"),
 ]
