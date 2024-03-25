9f60b89b8bf0bdac96fd2f083e3258453d7c52f1
Alexander Yurin <hiimmandarin32@gmail.com>
2024-03-25 16:50:47 +0300

    Fix

--- service/app/views.py
+++ service/app/views.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 from django.db.models import Prefetch
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import redirect, render
 from django.views import View
 
@@ -11,21 +12,29 @@ from app.models import OrderItems, Product, ProductPlacement
 
 
 class get_total_orders(View):
-	def get(self, request):
+	def get(self, request: HttpRequest) -> HttpResponse:
 		orders_ids = map(int, request.GET.keys())
 
 		data = OrderItems.objects.filter(order_id__in=orders_ids).select_related("order").prefetch_related(
 			Prefetch("product", queryset=Product.objects.prefetch_related("product_placements"))
 		)
+		if not data.exists():
+			return render(request, "main.html", context={"no_order": "Заказы отсутствуют!"})
+
 		order = defaultdict(list)
 		for product in data:
 			id_product = product.product.id
 			product_title = product.product.title
 			quantity = product.quantity
 			order_id = product.order.id
-			racks = product.product.racks.all()
+			racks = product.product.product_placements.all()
 			if racks.exists():
-				subracks = " ".join(filter(lambda x: not x.main, racks))
-				order[next(filter(lambda x: x.main, racks))].append((id_product, quantity, product_title, order_id, subracks))
+				main_rack = ""
+				additional_rack = []
+				for i in range(len(racks)):
+					if racks[i].main_rack:
+						main_rack = racks[i].rack
+					additional_rack.append(racks[i].rack)
+				order[main_rack].append((id_product, quantity, product_title, order_id, ", ".join(additional_rack)))
 
 		return render(request, "main.html", context={"order": order.items()})
