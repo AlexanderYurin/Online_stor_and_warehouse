d0f5ea765fefccbb79c80bd8e0b2bc52a9ca4331
Alexander Yurin <hiimmandarin32@gmail.com>
2024-03-25 17:28:30 +0300

    Добавлены tests.py

--- service/app/tests.py
+++ service/app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class YourTestCase(TestCase):
+	fixtures = ["data.json"]
+
+	def test_get_total_no_orders(self):
+		response = self.client.get(reverse("main"))
+
+	def test_get_total_orders(self):
+		response = self.client.get(reverse("main"), data={1: "1"})
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "заказ №1")
